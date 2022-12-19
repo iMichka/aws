6d59c21defff345a53a41e14585febb1cfb09ec0
Michka Popoff <michkapopoff@gmail.com>
2022-12-19 21:20:32 +0100

    Store tf state in aws, change backend

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -13,6 +13,41 @@ provider "aws" {
   region = "eu-west-3"
 }
 
+resource "aws_s3_bucket" "imichka-terraform-state" {
+  bucket = "imichka-terraform-state"
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_bucket_versioning" {
+  bucket = aws_s3_bucket.imichka-terraform-state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "dynamodb_terraform_state_lock" {
+  name           = "dynamodb_terraform_state_lock"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+  tags = {
+    Name = "DynamoDB Terraform State Lock Table"
+  }
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    encrypt        = true
+    bucket         = "imichka-terraform-state"
+    key            = "tfstate-s3-bucket"
+    region         = "eu-west-3"
+    dynamodb_table = "dynamodb_terraform_state_lock"
+  }
+}
+
 resource "aws_vpc" "test-env" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
