b5d2ffe23d611382a4d89441d17ef2607591021c
Michka Popoff <michkapopoff@gmail.com>
2023-02-06 21:07:50 +0100

    Add new subnet for mastodon experiment

--- prd/subnet.tf
+++ prd/subnet.tf
@@ -12,6 +12,24 @@ resource "aws_subnet" "public" {
   availability_zone = "${var.region}a"
   vpc_id            = aws_vpc.main.id
   tags = {
-    Name = "subnet-public-1"
+    Name = "subnet-public-2"
+  }
+}
+
+resource "aws_subnet" "private-mastodon" {
+  cidr_block        = "10.0.3.0/24"
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "${var.region}a"
+  tags = {
+    Name = "subnet-private-3"
+  }
+}
+
+resource "aws_subnet" "public-mastodon" {
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "${var.region}a"
+  vpc_id            = aws_vpc.main.id
+  tags = {
+    Name = "subnet-public-4"
   }
 }
