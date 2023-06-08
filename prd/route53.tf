fe9a2317006ba3916d3a524d222a3174f62d0f76
Michka Popoff <michkapopoff@gmail.com>
2023-06-08 22:49:06 +0200

    Add route 53 entries

---
+++ prd/route53.tf
@@ -0,0 +1,11 @@
+resource "aws_route53_zone" "imichka" {
+  name = "imichka.me"
+}
+
+resource "aws_route53_record" "mastodon" {
+  zone_id = aws_route53_zone.imichka.zone_id
+  name    = "mastodon.imichka.me"
+  type    = "A"
+  ttl     = 300
+  records = [aws_eip.mastodon-public-eip.public_ip]
+}
