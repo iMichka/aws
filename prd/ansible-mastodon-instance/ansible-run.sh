4e203e64a9131bc5cd1fce7be304f0a5c305ac95
Michka Popoff <michkapopoff@gmail.com>
2023-06-29 21:41:38 +0200

    Ansible: setup mastodon instance

--- prd/ansible-mastodon-instance/ansible-run.sh
+++ prd/ansible-mastodon-instance/ansible-run.sh
@@ -1,18 +1,44 @@
+#!/bin/bash -e
+
+if [ -z "$GITHUB_SHA" ]; then
+  GITHUB_SHA="main"
+fi
+echo "Github sha: $GITHUB_SHA"
+
 INSTANCE_ID=$(aws ec2 describe-instances --filters 'Name=tag:Name,Values=mastodon-instance' 'Name=instance-state-name,Values=running' --output text --query 'Reservations[*].Instances[*].InstanceId')
-echo $INSTANCE_ID
 
-# # Run a playbook
 COMMAND_ID=$( \
-	aws ssm send-command --document-name "AWS-RunRemoteScript" \
-						 --output-s3-bucket-name imichka-ansible \
-						 --output-s3-key-prefix output \
-						 --output-s3-region eu-west-3 \
-						 --targets "Key=instanceids,Values=$INSTANCE_ID"\
-						 --parameters '{"sourceType":["GitHub"],"sourceInfo":["{\"owner\" : \"iMichka\", \"repository\":\"aws\", \"getOptions\":\"branch:main\", \"path\":\"prd/ansible-mastodon-instance/hello-world.yaml\"}"], "commandLine":["ansible-playbook -i “localhost,” --check -c local hello-world.yaml"]}'\
-						 --timeout-seconds 600 --max-concurrency "50" --max-errors "0" --output text --query "Command.CommandId")
+  aws ssm send-command --document-name "AWS-RunRemoteScript" \
+             --output-s3-bucket-name imichka-ansible \
+             --output-s3-key-prefix output \
+             --targets "Key=instanceids,Values=$INSTANCE_ID"\
+             --parameters '{"sourceType":["GitHub"],"sourceInfo":["{\"owner\" : \"iMichka\", \"repository\":\"aws\", \"getOptions\":\"branch:'"$GITHUB_SHA"'\", \"path\":\"prd/ansible-mastodon-instance/setup-mastodon.yaml\"}"], "commandLine":["ansible-playbook -i “localhost,” setup-mastodon.yaml"]}'\
+             --timeout-seconds 600 --max-concurrency "50" --max-errors "0" --output text --query "Command.CommandId")
 
-echo $COMMAND_ID
+echo "Command id: $COMMAND_ID"
 
-aws ssm list-command-invocations \
-    --command-id $COMMAND_ID \
-    --details
+STATUS="None"
+x=1
+while [ "$STATUS" != "Success" ]
+do
+  RESULT=$(aws ssm list-command-invocations --command-id "$COMMAND_ID")
+  STATUS="$(echo "$RESULT" | jq ".CommandInvocations[0].Status")"
+  if [ "$STATUS" == '"InProgress"' ]; then
+    echo "Waiting $x times"
+    x=$(( x + 1 ))
+    sleep 5
+  else
+    if [ "$STATUS" != '"Success"' ]; then
+      echo "Ansible command failed"
+      echo "$RESULT"
+      aws s3 cp "s3://imichka-ansible/output/$COMMAND_ID/$INSTANCE_ID/awsdownloadContent/downloadContent/stdout" - || true
+      aws s3 cp "s3://imichka-ansible/output/$COMMAND_ID/$INSTANCE_ID/awsdownloadContent/downloadContent/stderr" - || true
+      aws s3 cp "s3://imichka-ansible/output/$COMMAND_ID/$INSTANCE_ID/awsrunShellScript/runShellScript/stdout" - || true
+      aws s3 cp "s3://imichka-ansible/output/$COMMAND_ID/$INSTANCE_ID/awsrunShellScript/runShellScript/stderr" - || true
+      exit 1
+    fi
+    echo "Ansible done"
+    aws s3 cp "s3://imichka-ansible/output/$COMMAND_ID/$INSTANCE_ID/awsrunShellScript/runShellScript/stdout" -
+    exit 0
+  fi
+done
--- prd/ec2.tf
+++ prd/ec2.tf
@@ -7,6 +7,10 @@ resource "aws_instance" "mastodon-instance" {
     device_index         = 0
   }
 
+  root_block_device {
+    volume_size = 30
+  }
+
   iam_instance_profile = aws_iam_instance_profile.mastodon-instance-profile.name
 
   user_data = <<EOF
@@ -20,6 +24,43 @@ rm amazon-ssm-agent.deb
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt update
 sudo apt install -y ansible awscli
+
+# https://www.mattburkedev.com/adventures-in-mastodon-self-hosting-the-story-so-far/#more
+# https://github.com/futurice/terraform-examples/blob/master/aws/aws_ec2_ebs_docker_host/provision-swap.sh
+# Better add a swap file to compile mastodon / run it
+
+SWAP_FILE_SIZE=4G
+SWAPPINESS=10
+
+echo "Setting up a swap file (size: $SWAP_FILE_SIZE, swappiness: $SWAPPINESS)..."
+
+# Create the swap file
+sudo fallocate -l $${SWAP_FILE_SIZE} /swapfile
+
+# Only root should be able to access to this file
+sudo chmod 600 /swapfile
+
+# Define the file as swap space
+sudo mkswap /swapfile
+
+# Enable the swap file, allowing the system to start using it
+sudo swapon /swapfile
+
+# Make the swap file permanent, otherwise, previous settings will be lost on reboot
+# Create a backup of the existing fstab, JustInCase(tm)
+sudo cp /etc/fstab /etc/fstab.bak
+# Add the swap file information at the end of the fstab
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+# Adjust the swappiness
+# With the default value of 10, the host will use swap if it has almost no other choice. Value is between 0 and 100.
+# 100 will make the host use the swap as much as possible, 0 will make it use only in case of emergency.
+# As swap access is slower than RAM access, having a low value here for a server is better.
+sudo sysctl vm.swappiness=$${SWAPPINESS}
+
+# Make this setting permanent, to not lose it on reboot
+sudo cp /etc/sysctl.conf /etc/sysctl.conf.bak
+echo "vm.swappiness=$${SWAPPINESS}" | sudo tee -a /etc/sysctl.conf
   EOF
 
   tags = {
