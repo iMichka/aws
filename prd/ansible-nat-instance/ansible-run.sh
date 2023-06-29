1973262c8556efb52dea1c7c602028eee411adea
Michka Popoff <michkapopoff@gmail.com>
2023-06-29 21:36:32 +0200

    Move files

--- prd/ansible-nat-instance/ansible-run.sh
+++ prd/ansible-nat-instance/ansible-run.sh
@@ -12,7 +12,7 @@ COMMAND_ID=$( \
              --output-s3-bucket-name imichka-ansible \
              --output-s3-key-prefix output \
              --targets "Key=instanceids,Values=$INSTANCE_ID"\
-             --parameters '{"sourceType":["GitHub"],"sourceInfo":["{\"owner\" : \"iMichka\", \"repository\":\"aws\", \"getOptions\":\"branch:'"$GITHUB_SHA"'\", \"path\":\"prd/ansible-nat-instance/reverse-proxy.yaml\"}"], "commandLine":["ansible-playbook -i “localhost,” reverse-proxy.yaml"]}'\
+             --parameters '{"sourceType":["GitHub"],"sourceInfo":["{\"owner\" : \"iMichka\", \"repository\":\"aws\", \"getOptions\":\"branch:'"$GITHUB_SHA"'\", \"path\":\"prd/ansible-nat-instance/setup-reverse-proxy.yaml\"}"], "commandLine":["ansible-playbook -i “localhost,” setup-reverse-proxy.yaml"]}'\
              --timeout-seconds 600 --max-concurrency "50" --max-errors "0" --output text --query "Command.CommandId")
 
 echo "Command id: $COMMAND_ID"
