f5ec9a6aea2975543d6783b8a5c21864f8408b9b
Michka Popoff <michkapopoff@gmail.com>
2023-09-20 12:12:06 +0200

    s3: add new rules
    Delete backups after 30 days

--- prd/s3.tf
+++ prd/s3.tf
@@ -4,14 +4,36 @@ resource "aws_s3_bucket" "imichka-mastodon" {
 
 resource "aws_s3_bucket" "imichka-ansible" {
   bucket = "imichka-ansible"
+}
 
-  lifecycle_rule {
-    id      = "logs-7-days-retention"
-    prefix  = "output"
-    enabled = true
+resource "aws_s3_bucket_lifecycle_configuration" "imichka-ansible-s3-configuration" {
+  bucket = aws_s3_bucket.imichka-ansible.id
+
+  rule {
+    id = "logs-7-days-retention"
+
+    filter {
+      prefix = "output/"
+    }
 
     expiration {
       days = 7
     }
+
+    status = "Enabled"
+  }
+
+  rule {
+    id = "backup-30-days-retention"
+
+    filter {
+      prefix = "backup-postgres/"
+    }
+
+    expiration {
+      days = 30
+    }
+
+    status = "Enabled"
   }
 }
