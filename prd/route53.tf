81b998e2a42e2fd937673008e2d9ff41e56b0001
Michka Popoff <michkapopoff@gmail.com>
2023-09-20 12:37:20 +0200

    route53: add new files subdomain

--- prd/route53.tf
+++ prd/route53.tf
@@ -9,3 +9,11 @@ resource "aws_route53_record" "mastodon" {
   ttl     = 300
   records = [aws_eip.mastodon-public-eip.public_ip]
 }
+
+resource "aws_route53_record" "files" {
+  zone_id = aws_route53_zone.imichka.zone_id
+  name    = "files.imichka.me"
+  type    = "A"
+  ttl     = 300
+  records = [aws_eip.mastodon-public-eip.public_ip]
+}
